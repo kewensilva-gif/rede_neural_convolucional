819b3ec930394f8c3310f82ade8dee5f5fe9fbd7
marly <marlyssonsd@gmail.com>
2025-06-16 00:22:06 -0300

    Atualização da segmentação

--- detect-segment/det_seg.py
+++ detect-segment/det_seg.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+variacao = 20  # Variacao de cor para a detecção de objetos
+
 def mostrarImagem(image, titulo):
   if len(image.shape) == 3 and image.shape[2] == 3:  # Imagem colorida
       # Testa um pixel para verificar se está em HSV
@@ -91,84 +93,112 @@ def removeRuido(mask,deltaRuido):
   # Filtro Mediano - Remover ruídos da máscara
   return cv2.medianBlur(mask, deltaRuido)
 
-def segObjetos(imagem_caminho, pasta_saida, nomeImagem, variacao=20, deltaRuido=5, tamSeg=200):
-  # Carrega a imagem local
-  imagem = carregaImagem(imagem_caminho)
-  if imagem is None:
-      print("Erro ao carregar a imagem. Verifique o caminho.")
-      return
+def segObjetos(imagem_caminho, pasta_saida, nomeImagem, deltaRuido=3, tamSeg=200):
+    multCoordenadas = []
+    global variacao
+    # Carrega a imagem local
+    imagem = carregaImagem(imagem_caminho)
+    if imagem is None:
+        print("Erro ao carregar a imagem. Verifique o caminho.")
+        return
 
-  # Cria a pasta de saída, se não existir
-  if not os.path.exists(pasta_saida):
-      os.makedirs(pasta_saida)
+    # Cria a pasta de saída, se não existir
+    if not os.path.exists(f"binary_{pasta_saida}"):
+        os.makedirs(f"binary_{pasta_saida}")
+        
+    if not os.path.exists(f"binary_view_{pasta_saida}"):
+        os.makedirs(f"binary_view_{pasta_saida}")
 
-  # Converte a imagem para HSV
-  imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_RGB2HSV)
+    if not os.path.exists(f"colored_{pasta_saida}"):
+        os.makedirs(f"colored_{pasta_saida}")
 
-  # Definição das Máscaras ref. aos Objetos
-  # Máscara 1
-  rgb = [196, 100, 117]
-  mask1 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,1)
-  # Máscara 2
-  rgb = [200, 90, 127]
-  mask2 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,2)
-  # Máscara 3
-  rgb = [183, 105, 129]
-  mask3 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,3)
-  # Máscara 4
-  rgb = [174, 87, 105]
-  mask4 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,4)
+    # Converte a imagem para HSV
+    imagem_hsv = cv2.cvtColor(imagem, cv2.COLOR_RGB2HSV)
 
-  # Combina as máscaras e apresenta
-  mask_red = cv2.add(mask1, mask2)
-  mask_red = cv2.add(mask_red, mask3)
-  mask_red = cv2.add(mask_red, mask4)
+    # Definição das Máscaras ref. aos Objetos
+    # Máscara 1
+    # rgb = [196, 125, 137]
+    # mask1 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,1)
+    # # Máscara 2
+    # rgb = [198, 131, 127]
+    # mask2 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,2)
+    # # Máscara 3
+    # rgb = [173, 105, 129]
+    # mask3 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,3)
+    # # Máscara 4
+    # rgb = [154, 87, 105]
+    # mask4 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,4)
 
-  # Aplica a máscara na imagem original
-  imagem_vermelho = cv2.bitwise_and(imagem, imagem, mask=mask_red)
 
-  # Filtro Mediano - Remover ruídos da máscara
-  mask_red = removeRuido(mask_red, deltaRuido)
+    # Máscara 1
+    rgb = [196, 100, 117]
+    mask1 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,1)
+    # Máscara 2
+    rgb = [200, 90, 127]
+    mask2 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,2)
+    # Máscara 3
+    rgb = [183, 105, 129]
+    mask3 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,3)
+    # Máscara 4
+    rgb = [174, 87, 105]
+    mask4 = definirMascaraOBJ(pasta_saida,imagem,imagem_hsv,rgb,4)
 
-  # Encontra os contornos dos objetos vermelhos
-  contornos, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  # Segmenta cada objeto vermelho encontrado
-  for i, contorno in enumerate(contornos):
 
-      # Cria uma máscara vazia do mesmo tamanho da imagem original
-      mask_objeto = np.zeros_like(mask_red)
+    # Combina as máscaras e apresenta
+    mask_red = cv2.add(mask1, mask2)
+    mask_red = cv2.add(mask_red, mask3)
+    mask_red = cv2.add(mask_red, mask4)
 
-      # Desenha o contorno na máscara
-      cv2.drawContours(mask_objeto, [contorno], -1, (255), thickness=cv2.FILLED)
+    # Aplica a máscara na imagem original
+    imagem_vermelho = cv2.bitwise_and(imagem, imagem, mask=mask_red)
 
-      # Normaliza para 0 e 1
-      mask_objeto_binario = (mask_objeto > 0).astype(np.uint8)
+    # Filtro Mediano - Remover ruídos da máscara
+    mask_red = removeRuido(mask_red, deltaRuido)
 
-      #-----------------------------------------------------------------------------------
-      # Obtém o retângulo delimitador para cada contorno
-      x, y, w, h = cv2.boundingRect(contorno)
+    # Aplicar operação morfológica CLOSE para conectar componentes próximos
+    kernel = np.ones((5,5), np.uint8)
+    mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_CLOSE, kernel)
 
-      # Extrai a região do objeto da imagem original
-      objeto = imagem[y:y+h, x:x+w]
+    # Encontra os contornos dos objetos vermelhos
+    contornos, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-      # Obtém as dimensões da imagem (altura, largura, canais)
-      altura, largura, _ = objeto.shape
-      total_pixels = altura * largura  # Número total de pixels
+    # Segmenta cada objeto vermelho encontrado
+    for i, contorno in enumerate(contornos):
 
-      # Condição para considerar somente segmentos com total_pixels > n
-      if total_pixels > 200:
-        # Salva o objeto como um arquivo separado
-        salvarImagem(f"{pasta_saida}{nomeImagem}_{i+1}_seg.png",objeto) # Salva como imagem binária (0 e 255)
+        # Obtém o retângulo delimitador para cada contorno
+        x, y, w, h = cv2.boundingRect(contorno)
 
-        #-----------------------------------------------------------------------------------
-        # Recorta o segmento binário com base no retângulo delimitador
-        segmento_recortado = mask_objeto[y:y+h, x:x+w]
-        segmento_binario = (segmento_recortado > 0).astype(np.uint8)
-      else :
-        print(f"Objeto {i+1}: não considerado por tratar-se de um possível ruído (total pixels {total_pixels}px)")
+        
+        # Extrai a região do objeto da imagem original
+        objeto = imagem[y:y+h, x:x+w]
+        # mostrarImagem(objeto, "objeto")
 
-  print(f"Processamento concluído! Objetos salvos em: {pasta_saida}")
+        # Obtém as dimensões da imagem (altura, largura, canais)
+        altura, largura, _ = objeto.shape
+        total_pixels = altura * largura  # Número total de pixels
+        
+        # Condição para considerar somente segmentos com total_pixels > n
+        if total_pixels > 1000 and altura > 20 and largura > 20:
+            
+            # Usa a máscara detalhada da detecção de cor
+            segmento_mascara = mask_red[y:y+h, x:x+w]
+
+            # Cria a imagem final com transparência
+            objeto_rgba = cv2.cvtColor(objeto, cv2.COLOR_RGB2RGBA)
+            objeto_rgba[:, :, 3] = segmento_mascara
+            # Converte de RGBA para BGRA para que o OpenCV salve as cores corretamente
+            objeto_bgra = cv2.cvtColor(objeto_rgba, cv2.COLOR_RGBA2BGRA)
+
+            segmento_binario = (segmento_mascara > 0).astype(np.uint8)
+            # Salva os 3 tipos de imagem
+            salvarImagem(f"colored_{pasta_saida}{nomeImagem}_{i+1}_seg.png", objeto_bgra)
+            salvarImagem(f"binary_view_{pasta_saida}{nomeImagem}_{i+1}_seg.png", segmento_mascara)
+            salvarImagem(f"binary_{pasta_saida}{nomeImagem}_{i+1}_seg.png", segmento_binario)
+            
+            multCoordenadas.append({'x': x, 'y': y, 'w': w, 'h': h, 'recorte': f"binary_{pasta_saida}{nomeImagem}_{i+1}_seg.png"})
+        else :
+            print(f"Objeto {i+1}: não considerado por tratar-se de um possível ruído (total pixels {total_pixels}px)")
 
 
 # Vetor de nome de imagens
@@ -182,14 +212,12 @@ for i in range(0,12):
     caminho_saida = f"detect-segment/imagens/{arquivo}{i}"
     caminho_imagem = f"detect-segment/imagens/{arquivo}{i}.{extensao}"
 
-    # Parâmetro para variação para mais ou menos referente à cor HSV do pixel do objeto
-    variacao = 5
 
     # Parâmetro para o Filtro Mediano - Remover ruídos da máscara
-    deltaRuido = 5
+    deltaRuido = 3
 
     # Parâmetro para definir o tamanho mínimo, em pixels, de um segmento
     tamSeg = 200
 
     # Detectar Objetos
-    segObjetos(caminho_imagem,caminho_saida + "/", f"{arquivo}{i}",variacao,deltaRuido,tamSeg)
+    segObjetos(caminho_imagem,caminho_saida + "/", f"{arquivo}{i}",deltaRuido,tamSeg)
